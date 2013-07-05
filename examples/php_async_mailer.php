0772cef458dd0dd91deb4cfda858c72a9e4de402
Daniel Lowrey <rdlowrey@gmail.com>
2013-07-05 14:26:32 -0400

    Added async mailer example

---
+++ examples/php_async_mailer.php
@@ -0,0 +1,33 @@
+<?php
+
+use Amp\MultiProcess\PhpDispatcher,
+    Amp\MultiProcess\CallResult,
+    Amp\ReactorFactory;
+
+require dirname(__DIR__) . '/autoload.php';
+require __DIR__ . '/support_files/async_mailer/MyAsyncMailer.php';
+
+$reactor = (new ReactorFactory)->select();
+$include = __DIR__ . '/support_files/async_mailer/StaticMailWorker.php';
+$dispatcher = new PhpDispatcher($reactor, $include, $workerProcessesToSpawn = 8);
+
+$mailer = new MyAsyncMailer($dispatcher);
+
+$mailJobs = [
+    [$to ='test1@test.com', $from='address@me.com', $subject='my test', $body='body', $headers=[]],
+    [$to ='test2@test.com', $from='address@me.com', $subject='my test', $body='body', $headers=[]],
+    [$to ='test3@test.com', $from='address@me.com', $subject='my test', $body='body', $headers=[]],
+    [$to ='test4@test.com', $from='address@me.com', $subject='my test', $body='body', $headers=[]],
+    [$to ='test5@test.com', $from='address@me.com', $subject='my test', $body='body', $headers=[]],
+    [$to ='test6@test.com', $from='address@me.com', $subject='my test', $body='body', $headers=[]],
+    [$to ='test7@test.com', $from='address@me.com', $subject='my test', $body='body', $headers=[]],
+    [$to ='test8@test.com', $from='address@me.com', $subject='my test', $body='body', $headers=[]]
+];
+
+// Kick things off when the reactor starts
+$reactor->once(function() use ($mailer, $mailJobs) {
+    $mailer->dispatch($mailJobs);
+});
+
+// Release the hounds! This should always be the last thing that happens in the script.
+$reactor->run();
---
+++ examples/support_files/async_mailer/MyAsyncMailer.php
@@ -0,0 +1,56 @@
+<?php
+
+use Amp\MultiProcess\PhpDispatcher,
+    Amp\MultiProcess\CallResult;
+
+class MyAsyncMailer {
+    
+    private $dispatcher;
+    private $pendingCalls = [];
+    
+    function __construct(PhpDispatcher $dispatcher) {
+        $this->dispatcher = $dispatcher;
+    }
+    
+    function dispatch(array $mailJobs) {
+        $onCallResult = function($result) { $this->onCallResult($result); };
+        
+        foreach ($mailJobs as $mailJob) {
+            list($to, $from, $subject, $body, $headers) = $mailJob;
+            
+            $callId = $this->dispatcher->call(
+                $onCallResult,
+                'StaticMailWorker::send',
+                $to,
+                $from,
+                $subject,
+                $body,
+                $headers
+            );
+            
+            $this->pendingCalls[$callId] = $mailJob;
+        }
+    }
+    
+    private function onCallResult(CallResult $callResult) {
+        $callId = $callResult->getCallId();
+        $mailJob = $this->pendingCalls[$callId];
+        
+        unset($this->pendingCalls[$callId]);
+        
+        list($to, $from, $subject, $body, $headers) = $mailJob;
+        
+        if ($callResult->isSuccess()) {
+            echo "Great success! {$to}; {$subject}", PHP_EOL;
+        } else {
+            echo "Mail fail! {$to} :(", PHP_EOL;
+        }
+        
+        if (empty($this->pendingCalls)) {
+            // If we don't stop the event reactor the program will run forever!
+            $this->dispatcher->stopReactor();
+        }
+    }
+    
+}
+
