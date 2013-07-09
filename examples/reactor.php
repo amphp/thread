fc447af8a8234ba9f23e1815ae8a7cd436b78cc0
Daniel Lowrey <rdlowrey@gmail.com>
2013-07-09 14:13:25 -0400

    Updated examples

--- examples/reactor.php
+++ examples/reactor.php
@@ -6,31 +6,25 @@ $reactor = (new Amp\ReactorFactory)->select();
 
 stream_set_blocking(STDIN, FALSE);
 
-/**
- * Echo back the data each time there is readable data on STDIN
- */
+// Echo back the data each time there is readable data on STDIN
 $reactor->onReadable(STDIN, function($stdin, $trigger) {
     while ($line = fgets($stdin)) {
         echo "--- $line";
     }
 });
 
-
-/**
- * Stop the program after N seconds
- */
-define('SECONDS_TO_RUN', 10);
-
-$count = SECONDS_TO_RUN;
-$reactor->schedule(function() use ($reactor, &$count) {
-    if (--$count) {
-        echo "- countdown: $count\n";
+// Countdown for ten seconds
+$secondsRemaining = 10;
+$reactor->schedule(function() use ($reactor, &$secondsRemaining) {
+    if (--$secondsRemaining) {
+        echo "- countdown: $secondsRemaining\n";
     } else {
         $reactor->stop();
     }
 }, $delay = 1);
 
-echo "Each input line will be echoed back to you for the next ", SECONDS_TO_RUN, " seconds ...\n\n";
+echo "Each input line will be echoed back to you for the next {$secondsRemaining} seconds ...\n\n";
 
+// Calling Reactor::run() will give control of program execution to the event reactor. The program
+// will not go continue beyond the next line until your code explicity calls Reactor::stop().
 $reactor->run();
-
