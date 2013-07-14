2b4252ee7a8de42d67ab1a7062fae4ab2e2b5236
Daniel Lowrey <rdlowrey@gmail.com>
2013-07-13 23:54:12 -0400

    Added docs, state accessor methods

--- src/Amp/Subscription.php
+++ src/Amp/Subscription.php
@@ -4,8 +4,8 @@ namespace Amp;
 
 class Subscription {
     
-    const DISABLED = 0;
     const ENABLED = 1;
+    const DISABLED = 0;
     const CANCELLED = -1;
     
     private $reactor;
@@ -15,17 +15,30 @@ class Subscription {
         $this->reactor = $reactor;
     }
     
+    /**
+     * Enable the previously-disabled subscription
+     * 
+     * If the subscription is already enabled this method has no effect.
+     * 
+     * @throws \RuntimeException If the subscription was previously cancelled
+     * @return void
+     */
     function enable() {
         if ($this->status === self::DISABLED) {
             $this->reactor->enable($this);
             $this->status = self::ENABLED;
-        } elseif ($this->status == self::CANCELLED) {
+        } elseif ($this->status === self::CANCELLED) {
             throw new \RuntimeException(
                 'Cannot reenable a subscription after cancellation'
             );
         }
     }
     
+    /**
+     * Temporarily disable this subscription
+     * 
+     * @return void
+     */
     function disable() {
         if ($this->status === self::ENABLED) {
             $this->reactor->disable($this);
@@ -33,6 +46,13 @@ class Subscription {
         }
     }
     
+    /**
+     * Permanently cancel this subscription
+     * 
+     * Once a subscription is cancelled it cannot be reenabled.
+     * 
+     * @return void
+     */
     function cancel() {
         if ($this->status !== self::CANCELLED) {
             $this->reactor->cancel($this);
@@ -40,5 +60,30 @@ class Subscription {
         }
     }
     
+    /**
+     * Is the subscription currently enabled?
+     * 
+     * @return bool
+     */
+    function isEnabled() {
+        return ($this->status === self::ENABLED);
+    }
+    
+    /**
+     * Is the subscription currently disabled?
+     * 
+     * @return bool
+     */
+    function isDisabled() {
+        return ($this->status === self::DISABLED);
+    }
+    
+    /**
+     * Has the subscription been cancelled?
+     * 
+     * @return bool
+     */
+    function isCancelled() {
+        return ($this->status === self::CANCELLED);
+    }
 }
-
