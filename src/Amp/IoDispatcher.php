48c496860d2686109b4824a0ca147e407f22bad2
Daniel Lowrey <rdlowrey@gmail.com>
2013-08-08 18:28:18 -0400

    Improved confusing variable names and conditionals

--- src/Amp/IoDispatcher.php
+++ src/Amp/IoDispatcher.php
@@ -5,63 +5,61 @@ namespace Amp;
 use Alert\Reactor;
 
 class IoDispatcher extends UnserializedIoDispatcher {
-    
-    function __construct(Reactor $reactor, $workerCmd = '', $poolSize = 1) {
-        $workerCmd = trim($workerCmd);
-        
-        $userInclude = dirname(dirname(__DIR__)) . '/workers/php/worker.php';
-        
-        if (!$workerCmd || (file_exists($workerCmd) && is_readable($workerCmd))) {
-            $workerCmd = $this->buildWorkerCmdFromFunctionFile($userInclude, $workerCmd);
-        } else {
-            throw new \InvalidArgumentException(
-                "User include file does not exist: {$workerCmd}"
-            );
+
+    function __construct(Reactor $reactor, $userFunctionFile = '', $poolSize = 1) {
+        $userFunctionFile = trim($userFunctionFile);
+
+        if ($userFunctionFile) {
+            $this->validateUserFunctionFile($userFunctionFile);
         }
-        
+
+        $baseWorkerScript = dirname(dirname(__DIR__)) . '/workers/php/worker.php';
+        $workerCmd = $this->buildWorkerCommand($baseWorkerScript, $userFunctionFile);
+
         $crf = new CallResultFactory;
-        
+
         parent::__construct($reactor, $workerCmd, $poolSize, $crf);
     }
-    
-    private function buildWorkerCmdFromFunctionFile($userInclude, $workerCmd) {
-        if ($workerCmd) {
-            $this->validateWorkerLint($workerCmd);
+
+    private function validateUserFunctionFile($userFunctionFile) {
+        if (!(file_exists($userFunctionFile) && is_readable($userFunctionFile))) {
+            throw new \InvalidArgumentException(
+                "User function file does not exist: {$userFunctionFile}"
+            );
         }
-        
+
+        $cmd = PHP_BINARY . ' -l ' . $userFunctionFile . '  && exit';
+        exec($cmd, $outputLines, $exitCode);
+
+        if ($exitCode) {
+            throw new \RuntimeException(
+                "User function file failed lint test: " . PHP_EOL . implode(PHP_EOL, $outputLines)
+            );
+        }
+    }
+
+    private function buildWorkerCommand($baseWorkerScript, $userFunctionFile) {
         $cmd = [];
         $cmd[] = PHP_BINARY;
         if ($ini = get_cfg_var('cfg_file_path')) {
             $cmd[] = "-c $ini";
         }
         $cmd[] = $userInclude;
-        
+
         if ($workerCmd) {
             $cmd[] = $workerCmd;
         }
-        
+
         return implode(' ', $cmd);
     }
-    
-    private function validateWorkerLint($workerCmd) {
-        $cmd = PHP_BINARY . ' -l ' . $workerCmd . '  && exit';
-        exec($cmd, $outputLines, $exitCode);
-        // @codeCoverageIgnoreStart
-        if ($exitCode) {
-            throw new \RuntimeException(
-                "Worker lint validation failed: " . PHP_EOL . implode(PHP_EOL, $outputLines)
-            );
-        }
-        // @codeCoverageIgnoreEnd
-    }
-    
+
     /**
      * Automatically serializes variable arguments for transport to PHP workers
-     * 
+     *
      * @param callable $onResult  The function to notify when the async result returns
      * @param string   $procedure The procedure to invoke asynchronously
      * @param mixed    $arg1, $arg2, ... $argN
-     * 
+     *
      * @return int Returns the call ID associated with this invocation
      */
     function call($onResult, $procedure, $varArgs = NULL) {
@@ -72,11 +70,10 @@ class IoDispatcher extends UnserializedIoDispatcher {
             unset($args[0], $args[1]);
             $args = array_values($args);
         }
-        
+
         $serializedArgs = serialize($args);
-        
+
         return parent::call($onResult, $procedure, $serializedArgs);
     }
-    
-}
 
+}
