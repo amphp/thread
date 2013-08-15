7edc69d2e81fb78f47bb36b1228663f2e0eb48d1
Daniel Lowrey <rdlowrey@gmail.com>
2013-08-15 16:13:59 -0400

    Moved convenience autoloader into vendor directory to play nice with composer

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -3,13 +3,13 @@
 spl_autoload_register(function($class) {
     if (0 === strpos($class, 'Amp\\')) {
         $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-        $file = __DIR__ . "/src/$class.php";
+        $file = __DIR__ . "/../src/$class.php";
         if (file_exists($file)) {
             require $file;
         }
     } elseif (0 === strpos($class, 'Alert\\')) {
         $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-        $file = __DIR__ . "/vendor/Alert/src/$class.php";
+        $file = __DIR__ . "/Alert/src/$class.php";
         if (file_exists($file)) {
             require $file;
         }
